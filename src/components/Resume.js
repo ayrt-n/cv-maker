2d08ddb7010969db20404cb79592b2a1799b5170
ayrton <ayrton.parkinson1@gmail.com>
2022-09-19 10:05:12 -0400

    Add Skills section to Resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
 import ResumeHeader from "./ResumeHeader";
+import Skills from "./Skills";
 import '../styles/Resume.css';
 
 class Resume extends Component {
   render() {
     return (
-      <div className="Resume">
+      <div className='Resume'>
         <ResumeHeader
-          name="Ayrton Parkinson"
-          location="Ottawa, ON, Canada"
-          phone="(613) 252-6800"
-          email="Ayrton.Parkinson1@gmail.com"
+          name='Ayrton Parkinson'
+          location='Ottawa, ON, Canada'
+          phone='(613) 252-6800'
+          email='Ayrton.Parkinson1@gmail.com'
+        />
+        <Skills
+          title='Skills'
+          skills={ ['Ruby', 'Ruby on Rails', 'HTML', 'CSS', 'JavaScript', 'React', 'Ruby', 'Ruby on Rails', 'HTML', 'CSS', 'JavaScript', 'React'] }
         />
       </div>
     );
