a6a71ecca2cd4b260e35500d2e626a333268c660
ayrton <ayrton.parkinson1@gmail.com>
2022-09-19 11:39:17 -0400

    Add Projects to Resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,6 +3,7 @@ import ResumeHeader from "./ResumeHeader";
 import Skills from "./Skills";
 import Education from "./Education";
 import Work from "./Work";
+import Projects from './Projects';
 
 import '../styles/Resume.css';
 
@@ -60,6 +61,32 @@ class Resume extends Component {
                 details: [
                   "Produced analysis and developed models to analyze the impact of economic policy to support the Minister of Finance with timely, well-informed research and advice, including policy proposals with significant financial implications ($100M+)",
                   "Streamlined and automated data analysis work to reduce time required for tasks and improve quality of analysis",
+                ]
+              }
+            ]
+          }
+        />
+        <Projects
+          projects={
+            [
+              {
+                name: "To-do List Web Application",
+                startDate: "August 2022",
+                endDate: "September 2022",
+                details: [
+                  "Developed to-do list tracking web application allowing users to create and keep track of projects using to-do lists",
+                  "Integrated Web Storage API to use local storage to store and persist user data across browser sessions",
+                  "Dynamic and mobile responsive front-end built with JavaScript, HTML, and CSS",
+                ]
+              },
+              {
+                name: "Social Media Web Application",
+                startDate: "May 2022",
+                endDate: "June 2022",
+                details: [
+                  "Designed, developed and deployed social media web application; application allows users to add friends and share, comment on, and like posts and photos with friends",
+                  "Wrote tests for backend and frontend features to ensure the application continued to work throughout development, speed up development time, and minimize the potential for bugs in production",
+                  "User uploads stored in cloud via Amazon S3. User welcome and confirmation emails sent through Twilio SendGrid API",
                 ]
               }
             ]
