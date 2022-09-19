0ca578f179022e90d681f880a9bfd566bb2de41e
ayrton <ayrton.parkinson1@gmail.com>
2022-09-19 11:25:02 -0400

    Add Work component to Resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import ResumeHeader from "./ResumeHeader";
 import Skills from "./Skills";
 import Education from "./Education";
+import Work from "./Work";
 
 import '../styles/Resume.css';
 
@@ -35,6 +36,34 @@ class Resume extends Component {
               endDate: "April 2016",
             }
           ]}
+        />
+        <Work
+          workExperience={
+            [
+              {
+                employerName: "Canada Deposit Insurance Corporation",
+                location: "Ottawa, ON",
+                startDate: "October 2020",
+                endDate: "Present",
+                jobTitle: "Risk Manager",
+                details: [
+                  "Developing internal tools in collaboration with data analytics team which help to improve efficiency in monitoring the economic environment and portfolio; internal tools have been widely adopted and integrated across the organization",
+                  "Managing a portfolio of medium- to large-sized banks; researching, analyzing, and presenting key findings and recommendations to senior management and stakeholders to identify, escalate, and address areas of concern",
+                ]
+              },
+              {
+                employerName: "Department of Finance Canada",
+                location: "Ottawa, ON",
+                startDate: "October 2017",
+                endDate: "October 2020",
+                jobTitle: "Economist",
+                details: [
+                  "Produced analysis and developed models to analyze the impact of economic policy to support the Minister of Finance with timely, well-informed research and advice, including policy proposals with significant financial implications ($100M+)",
+                  "Streamlined and automated data analysis work to reduce time required for tasks and improve quality of analysis",
+                ]
+              }
+            ]
+          }
         />
       </div>
     );
