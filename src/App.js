11db5072c94651d48c9b059d067cfd8488b3a512
ayrton <ayrton.parkinson1@gmail.com>
2022-09-19 09:36:42 -0400

    Add Resume component to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
+import Resume from "./components/Resume";
+import './styles/App.css';
 
 class App extends Component {
   render() {
       return (
-      <div>
+      <div className="App">
+        <Resume />
       </div>
     );
   }
