3738981eae137314070b297a4f81446526d2981a
ayrton <ayrton.parkinson1@gmail.com>
2022-09-21 14:15:51 -0400

    Move state upwards into generalized Form component

--- src/components/EducationForm.js
+++ src/components/EducationForm.js
@@ -7,17 +7,16 @@ class EducationForm extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      educationHistory: this.props.resumeContent.educationHistory,
-    }
-
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
     this.addEducation = this.addEducation.bind(this);
     this.removeEducation = this.removeEducation.bind(this);
   }
 
-  createNewEducationHistory() {
+  handleChange(e) {
+    this.props.handleChange(e);
+  }
+
+  createNewEducationObj() {
     return {
       school: "School Name",
       program: "Program",
@@ -28,57 +27,27 @@ class EducationForm extends Component {
     }
   }
 
-  handleInputChange(e) {
-    const educationId = e.target.closest('div.educationItem').id;
-    const { educationHistory } = this.state;
-
-    const targetIndex = educationHistory.findIndex((educationItem) => (educationItem.id === educationId));
-    const targetKey = e.target.dataset.objectKey;
-
-    if (targetIndex !== -1) {
-      educationHistory[targetIndex][targetKey] = e.target.value;
-      this.setState({
-        educationHistory
-      });
-    }
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-    this.props.handleSubmit(this.state.educationHistory);
-  }
-
   addEducation(e) {
     e.preventDefault();
-    this.setState({
-      educationHistory: this.state.educationHistory.concat(this.createNewEducationHistory())
-    })
+    this.props.addEducation(this.createNewEducationObj());
   }
 
   removeEducation(e) {
     e.preventDefault();
-    const educationId = e.target.closest('div').id;
-    const { educationHistory } = this.state;
-    const targetIndex = educationHistory.findIndex((educationItem) => (educationItem.id === educationId));
-
-    if (targetIndex !== -1) {
-      this.setState({
-        educationHistory: educationHistory.slice(0, targetIndex).concat(educationHistory.slice(targetIndex + 1))
-      });
-    }
+    this.props.removeEducation(e)
   }
 
   render() {
     return (
-      <form className="Form" onSubmit={ this.handleSubmit }>
-        { this.state.educationHistory.map((educationItem) => {
+      <div>
+        { this.props.educationHistory.map((educationItem, index) => {
             return (
-              <div key={ educationItem.id } id={ educationItem.id } className="educationItem">
-                <InputFormField value={ educationItem.school } label="School Name" id={ `school-${educationItem.id}` } objectKey="school" handleInputChange={ this.handleInputChange } />
-                <InputFormField value={ educationItem.program } label="Program" id={ `program-${educationItem.id}` } objectKey="program" handleInputChange={ this.handleInputChange } />
-                <InputFormField value={ educationItem.startDate } label="Start Date" id={ `start-${educationItem.id}` } objectKey="startDate" handleInputChange={ this.handleInputChange } />
-                <InputFormField value={ educationItem.endDate } label="End Date" id={ `end-${educationItem.id}` } objectKey="endDate" handleInputChange={ this.handleInputChange } />
-                <InputFormField value={ educationItem.location } label="Location" id={ `location-${educationItem.id}` } objectKey="location" handleInputChange={ this.handleInputChange } />
+              <div key={ educationItem.id } data-education-index={ index } className="educationItem">
+                <InputFormField value={ educationItem.school } label="School Name" id={ `school-${educationItem.id}` } objectKey="school" handleInputChange={ this.handleChange } />
+                <InputFormField value={ educationItem.program } label="Program" id={ `program-${educationItem.id}` } objectKey="program" handleInputChange={ this.handleChange } />
+                <InputFormField value={ educationItem.startDate } label="Start Date" id={ `start-${educationItem.id}` } objectKey="startDate" handleInputChange={ this.handleChange } />
+                <InputFormField value={ educationItem.endDate } label="End Date" id={ `end-${educationItem.id}` } objectKey="endDate" handleInputChange={ this.handleChange } />
+                <InputFormField value={ educationItem.location } label="Location" id={ `location-${educationItem.id}` } objectKey="location" handleInputChange={ this.handleChange } />
 
                 <button onClick={this.removeEducation}>Delete</button>
               </div>
@@ -86,8 +55,7 @@ class EducationForm extends Component {
           })
         }
         <button onClick={this.addEducation}>Add Education</button>
-        <button type="submit">Save</button>
-      </form>
+      </div>
     );
   }
 }
