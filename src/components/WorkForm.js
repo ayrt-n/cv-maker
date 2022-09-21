fe4af2902ee05ddf785f6c037c6f7653c3817e4b
ayrton <ayrton.parkinson1@gmail.com>
2022-09-21 10:28:26 -0400

    Add ability to add additional job description bullet points

--- src/components/WorkForm.js
+++ src/components/WorkForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import InputFormField from "./InputFormField";
 import '../styles/Form.css';
+import uniqid from 'uniqid';
 
 class WorkForm extends Component {
   constructor(props) {
@@ -12,6 +13,15 @@ class WorkForm extends Component {
 
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleNestedInputChange = this.handleNestedInputChange.bind(this);
+    this.addJobDescription = this.addJobDescription.bind(this);
+  }
+
+  createJobDescriptionBullet() {
+    return {
+      text: '',
+      id: uniqid(),
+    }
   }
 
   handleInputChange(e) {
@@ -29,9 +39,41 @@ class WorkForm extends Component {
     }
   }
 
+  handleNestedInputChange(e) {
+    const workId = e.target.closest('div.workItem').id;
+    const detailId = e.target.closest('div').id;
+    const { workExperience } = this.state;
+
+    const targetIndex = workExperience.findIndex((workItem) => (workItem.id === workId));
+    const targetSubIndex = workExperience[targetIndex]['details'].findIndex((detail) => (detail.id === detailId));
+    
+    if (targetIndex !== -1 && targetSubIndex !== -1) {
+      workExperience[targetIndex]['details'][targetSubIndex].text = e.target.value
+
+      this.setState({
+        workExperience
+      });
+    }
+  }
+
   handleSubmit(e) {
     e.preventDefault();
     this.props.handleSubmit(this.state.workExperience);
+  }
+
+  addJobDescription(e) {
+    const workId = e.target.closest('div.workItem').id;
+    const { workExperience } = this.state;
+
+    const targetIndex = workExperience.findIndex((workItem) => (workItem.id === workId));
+    
+    if (targetIndex !== -1) {
+      workExperience[targetIndex]['details'] = workExperience[targetIndex]['details'].concat(this.createJobDescriptionBullet());
+
+      this.setState({
+        workExperience
+      });
+    }
   }
 
   render() {
@@ -41,24 +83,21 @@ class WorkForm extends Component {
           return(
             <div key={ workItem.id } id={ workItem.id } className="workItem">
               <InputFormField value={workItem.employerName} label="Employer Name" id={`employer-${workItem.id}`} objectKey="employerName" handleInputChange={this.handleInputChange} />
-
               <InputFormField value={workItem.location} label="Location" id={`location-${workItem.id}`} objectKey="location" handleInputChange={this.handleInputChange} />
-
               <InputFormField value={workItem.startDate} label="Start Date" id={`start-${workItem.id}`} objectKey="startDate" handleInputChange={this.handleInputChange} />
-
               <InputFormField value={workItem.endDate} label="End Date" id={`end-${workItem.id}`} objectKey="endDate" handleInputChange={this.handleInputChange} />
 
               <label htmlFor={`details-${workItem.id}`}>Job Description</label>
               { workItem.details.map((detail) => {
                     return (
                       <div key={ detail.id } id={ detail.id }>
-                        <label htmlFor={`detail-${detail.id}`}>?</label>
-                        <input type="text" value={ detail.text } id={`details-${workItem.id}`} data-object-key="details" onChange={this.handleInputChange}></input>
+                        <input type="text" value={ detail.text } id={`details-${workItem.id}`} onChange={this.handleNestedInputChange}></input>
                       </div>
                     );
                   }  
                 )
               }
+              <button onClick={this.addJobDescription}>Add Job Description</button>
             </div>
           );
         })}
