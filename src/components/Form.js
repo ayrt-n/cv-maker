ba0585b721ebedf377fea20bf42362da49f3163f
ayrton <ayrton.parkinson1@gmail.com>
2022-09-23 09:21:31 -0400

    Replace individual remove section handler with general removeResumeSectionElement

--- src/components/Form.js
+++ src/components/Form.js
@@ -14,14 +14,12 @@ class Form extends Component {
 
     this.handleFormSubmit = this.handleFormSubmit.bind(this);
     this.handleHeaderChange = this.handleHeaderChange.bind(this);
-    this.removeSkill = this.removeSkill.bind(this);
-    this.removeEducation = this.removeEducation.bind(this);
     this.handleWorkDetailChange = this.handleWorkDetailChange.bind(this);
-    this.removeWorkExperience = this.removeWorkExperience.bind(this);
     this.addWorkDetail = this.addWorkDetail.bind(this);
     this.removeWorkDetail = this.removeWorkDetail.bind(this);
     this.handleResumeSectionChange = this.handleResumeSectionChange.bind(this);
     this.addResumeSectionElement = this.addResumeSectionElement.bind(this);
+    this.removeResumeSectionElement = this.removeResumeSectionElement.bind(this);
   }
 
   handleResumeSectionChange(e) {
@@ -42,6 +40,17 @@ class Form extends Component {
     this.setState({
       [targetSection]: this.state[targetSection].concat(obj)
     });
+  }
+
+  removeResumeSectionElement(e) {
+    const targetSection = e.target.closest('[data-section]').dataset.section;
+    const targetIndex = e.target.closest('[data-section-index]').dataset.skillIndex;
+    
+    this.setState(prevState => {
+      const resumeSection = [...prevState[targetSection]];
+      resumeSection.splice(targetIndex, 1);
+      return ({ [targetSection]: resumeSection })
+    })
   }
 
   handleFormSubmit(e) {
@@ -181,10 +190,10 @@ class Form extends Component {
     return (
       <form onSubmit={ this.handleFormSubmit }>
         <HeaderForm header={ header } handleChange={ this.handleHeaderChange } />
-        <SkillsForm skills={ skills } addSkill={ this.addResumeSectionElement } removeSkill={ this.removeSkill } />
-        <EducationForm educationHistory={ educationHistory } handleChange={ this.handleResumeSectionChange } addEducation={ this.addResumeSectionElement } removeEducation={ this.removeEducation } />
-        <WorkForm workExperience={ workExperience } handleChange={ this.handleResumeSectionChange } handleWorkDetailChange={ this.handleWorkDetailChange } addWorkExperience={ this.addResumeSectionElement } removeWorkExperience={ this.removeWorkExperience } addWorkDetail={ this.addWorkDetail } removeWorkDetail={ this.removeWorkDetail }/>
-        <ProjectForm projects={ projects } handleChange={ this.handleResumeSectionChange } addProject={ this.addResumeSectionElement } />
+        <SkillsForm skills={ skills } addSkill={ this.addResumeSectionElement } removeSkill={ this.removeResumeSectionElement } />
+        <EducationForm educationHistory={ educationHistory } handleChange={ this.handleResumeSectionChange } addEducation={ this.addResumeSectionElement } removeEducation={ this.removeResumeSectionElement } />
+        <WorkForm workExperience={ workExperience } handleChange={ this.handleResumeSectionChange } handleWorkDetailChange={ this.handleWorkDetailChange } addWorkExperience={ this.addResumeSectionElement } removeWorkExperience={ this.removeResumeSectionElement } addWorkDetail={ this.addWorkDetail } removeWorkDetail={ this.removeWorkDetail }/>
+        <ProjectForm projects={ projects } handleChange={ this.handleResumeSectionChange } addProject={ this.addResumeSectionElement } removeProject={ this.removeResumeSectionElement } />
         <button type="submit">Save</button>
       </form>
     );
