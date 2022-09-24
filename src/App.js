91682c48f002352b3dc6cc3f6151ee790c637a94
ayrton <ayrton.parkinson1@gmail.com>
2022-09-24 08:47:35 -0400

    Remove seed data

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import Resume from "./components/Resume";
 import Form from "./components/Form";
 import './App.css';
-import uniqid from 'uniqid';
 
 class App extends Component {
   constructor(props) {
@@ -12,92 +11,11 @@ class App extends Component {
 
     this.state = {
       resumeContent: {
-        header: {
-          name: 'Full Name',
-          location: 'Location',
-          phone: 'Phone #',
-          email: 'Email',
-        },
-        skills: [
-          {
-            text: 'Ruby',
-            id: uniqid(),
-          }
-        ],
-        educationHistory: [
-          {
-            school: "School Name",
-            program: "Program",
-            location: "School Location",
-            startDate: "Start Date",
-            endDate: "End Date",
-            id: uniqid(),
-          },
-          {
-            school: "School Name",
-            program: "Program",
-            location: "School Location",
-            startDate: "Start Date",
-            endDate: "End Date",
-            id: uniqid(),
-          }
-        ],
-        workExperience: [
-          {
-            employerName: "Employer Name",
-            location: "Employer Location",
-            startDate: "Start Date",
-            endDate: "End Date",
-            jobTitle: "Job Title",
-            details: [
-              {
-                text: "Job details (e.g., responsibilities, achievements, highlights)",
-                id: uniqid(),
-              }
-            ],
-            id: uniqid(),
-          },
-          {
-            employerName: "Employer Name",
-            location: "Employer Location",
-            startDate: "Start Date",
-            endDate: "End Date",
-            jobTitle: "Job Title",
-            details: [
-              {
-                text: "Job details (e.g., responsibilities, achievements, highlights)",
-                id: uniqid(),
-              }
-            ],
-            id: uniqid(),
-          }
-        ],
-        projects: [
-          {
-            name: "Project Name",
-            startDate: "Start Date",
-            endDate: "End Date",
-            details: [
-              {
-                text: "Project details (e.g., summary, achievments, lessons learned)",
-                id: uniqid(),
-              }
-            ],
-            id: uniqid(),
-          },
-          {
-            name: "Project Name",
-            startDate: "Start Date",
-            endDate: "End Date",
-            details: [
-              {
-                text: "Project details (e.g., summary, achievments, lessons learned)",
-                id: uniqid(),
-              }
-            ],
-            id: uniqid(),
-          }
-        ],
+        header: {},
+        skills: [],
+        educationHistory: [],
+        workExperience: [],
+        projects: [],
       }
     }
   }
