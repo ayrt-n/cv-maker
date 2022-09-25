993a0dbaf74ee6207b7568b810a0a33645aa3562
ayrton <ayrton.parkinson1@gmail.com>
2022-09-25 08:53:49 -0400

    Add edit resume button and associated onClick method

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -6,18 +6,31 @@ import Work from "./Work";
 import Projects from './Projects';
 
 import '../styles/Resume.css';
+import '../styles/Form.css';
 
 class Resume extends Component {
+  constructor(props) {
+    super(props);
+    this.editResume = this.editResume.bind(this);
+  }
+
+  editResume() {
+    this.props.editResume();
+  }
+
   render() {
     const { header, skills, educationHistory, workExperience, projects } = this.props.resumeContent;
 
     return (
-      <div className='Resume'>
-        <ResumeHeader headerContent={ header } />
-        <Skills skills={ skills } />
-        <Education educationHistory={ educationHistory } />
-        <Work workExperience={ workExperience } />
-        <Projects projects={ projects } />
+      <div className="Resume-container">
+        <button onClick={ this.editResume } className="Resume-edit button">Edit Resume</button>
+        <div className='Resume'>
+          <ResumeHeader headerContent={ header } />
+          <Skills skills={ skills } />
+          <Education educationHistory={ educationHistory } />
+          <Work workExperience={ workExperience } />
+          <Projects projects={ projects } />
+        </div>
       </div>
     );
   }
