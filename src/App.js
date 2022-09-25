5d5e5d9a38cc2d46a9685bd16b50e00e444d81f0
ayrton <ayrton.parkinson1@gmail.com>
2022-09-25 08:54:12 -0400

    Add ability to switch between active Components (Form and Resume)

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
     super(props);
 
     this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.editResume = this.editResume.bind(this);
 
     this.state = {
       resumeContent: {
@@ -16,21 +17,37 @@ class App extends Component {
         educationHistory: [],
         workExperience: [],
         projects: [],
-      }
+      },
+      formActive: true,
+      edit: false,
     }
   }
 
   handleFormSubmit(newResumeContent) {
     this.setState({
-      resumeContent: newResumeContent
+      resumeContent: newResumeContent,
+      formActive: false,
     })
   }
 
+  editResume() {
+    this.setState({
+      formActive: true,
+      edit: true,
+    });
+  }
+
   render() {
+      const activeComponent = this.state.formActive
+      ? <Form resumeContent={ this.state.resumeContent } handleFormSubmit={ this.handleFormSubmit } formActive={ this.state.formActive } />
+      : <Resume resumeContent={ this.state.resumeContent } resumeHidden={ this.state.formActive } editResume={ this.editResume } />;
+
       return (
       <div className="App">
-        <Form resumeContent={ this.state.resumeContent } handleFormSubmit={ this.handleFormSubmit } />
-        <Resume resumeContent={ this.state.resumeContent }/>
+        <h1 className="App-header">CV Maker</h1>
+        <div className="App-content-container">
+          { activeComponent }
+        </div>
       </div>
     );
   }
